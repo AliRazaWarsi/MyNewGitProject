3250e24b38dd4f3df64a8db16d99f7b3509a5344
Ali Raza <alimustafai401@gmail.com>
2023-10-04 11:21:48 +0500

    step-11

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -64,7 +64,7 @@ void main() {
       final user = provider.currentUser;
       expect(user, isNotNull);
       expect(user!.isEmailVerified, true);
-    });
+    }); 
 
     test('should be able to log out and log in again', () async {
       await provider.logOut();
